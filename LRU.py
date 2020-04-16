36c35e983e553e6c9681a0cb1413268a4e148738
akankshadhanewar <akanksha.dhanewar@gmail.com>
2020-04-17 00:38:37 +0530

    added methods

--- LRU.py
+++ LRU.py
@@ -1,7 +1,14 @@
 class LRU:
     # constructor
     def __init__(self,cache_capacity):
-        
+
         self.cache_capacity = cache_capacity
         self.data = {}
         self.cache = {}
+    
+    def put(self):
+        pass
+    def get(self):
+        pass 
+    def get_cache(self):
+        pass
