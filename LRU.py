01a01bd0bbe19ed6e14a1310fa80d463409a4953
akankshadhanewar <akanksha.dhanewar@gmail.com>
2020-04-17 01:45:33 +0530

    filled the methods with logic

--- LRU.py
+++ LRU.py
@@ -6,11 +6,21 @@ class LRU:
         self.cache = {}
         self.lru = {}
     
-    def put(self):
-        pass
+    def put(self, key, value):
+        if key not in self.cache and len(self.cache) == self.cache_capacity:
+            old_data = min(self.lru.keys(), key = lambda k:self.lru[k])
+            self.cache.pop(old_data)
+            self.lru.pop(old_data)
+        self.cache[key] = value
+        self.lru[key] = 1
+        return "put success"
 
-    def get(self):
-        pass
+    def get(self, key):
+        if key in self.cache:
+            self.lru[key] += 1
+            return self.cache[key]
+        else:
+            print("This key is not in cache")
 
     def get_cache(self):
-        pass
+        return self.cache
