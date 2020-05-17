60b3b0da6a71cd92f7ecdf2bf9f1f295aa2f40cc
akankshadhanewar <akanksha.dhanewar@gmail.com>
2020-05-18 00:43:57 +0530

    Added mark function and unmark function in js file

--- ToDo.js
+++ ToDo.js
@@ -16,7 +16,7 @@ function displayTask(){
         tbody_data = tbody_data+"<tr class='td_class'>";
         tbody_data = tbody_data+"<td>"+tasks[i].title+"</td>";
         tbody_data = tbody_data+"<td>"+tasks[i].desciption+"</td>";
-        tbody_data = tbody_data+'<td><button onclick="Delete('+i+')">Delete</button>&nbsp;&nbsp; <button onclick="mark('+i+')">Mark</button></td>';
+        tbody_data = tbody_data+'<td><button onclick="Delete('+i+')">Delete</button>&nbsp; <button onclick="mark('+i+')">Mark</button>&nbsp; <button onclick="unmark('+i+')">Unmark</button></td>';
         tbody_data = tbody_data+"</tr>";
     }
     document.getElementById("tbody").innerHTML = tbody_data;
@@ -25,3 +25,14 @@ function Delete(task){
     tasks.splice(task,1)
     displayTask();
 }
+
+function mark(task){
+    t = document.getElementsByClassName("td_class")
+    t[task].style.backgroundColor = "red"
+}
+
+function unmark(task){
+    t = document.getElementsByClassName("td_class")
+    t[task].style.backgroundColor = "#f5f5dc"
+}
+
