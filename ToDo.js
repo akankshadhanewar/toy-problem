091f7444548f76e2b91317f08fd60df1b5f4fdc6
akankshadhanewar <akanksha.dhanewar@gmail.com>
2020-05-18 00:14:32 +0530

    Added sript for displaying the tasks which are given

--- ToDo.js
+++ ToDo.js
@@ -8,4 +8,13 @@ function addTask(){
     }
     task = {title:t, desc:d}
     tasks.push(task)
+    tbody_data = "";
+		for(i=0;i<tasks.length;i++){
+			tbody_data = tbody_data+"<tr class='td_class'>";
+			tbody_data = tbody_data+"<td>"+tasks[i].title+"</td>";
+			tbody_data = tbody_data+"<td>"+tasks[i].desciption+"</td>";
+			tbody_data = tbody_data+'<td><button onclick="delete('+i+')">Delete</button>&nbsp;&nbsp; <button onclick="mark('+i+')">Mark</button></td>';
+			tbody_data = tbody_data+"</tr>";
+		}
+		document.getElementById("tbody").innerHTML = tbody_data;
 }
