ee947b5e0e0fa66ff21ffe69a436b2c739679e3b
akankshadhanewar <akanksha.dhanewar@gmail.com>
2020-05-20 13:15:21 +0530

    Added a class Todo and retuned a div which consists of input feilds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo />
     </div>
   );
 }
+
+
+class Todo extends React.Component{
+  constructor() {
+  super();
+  }
+  render(){
+    return(
+      <div>
+        <h1>Todo List</h1>
+        <input type="text" placeholder="You task here"></input>&nbsp;
+        <input type="date"></input>&nbsp;
+        <button>Add task</button>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
