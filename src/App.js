e501f8dd640ebff2ae6a085dddb9ea7641658898
akankshadhanewar <akanksha.dhanewar@gmail.com>
2020-05-20 14:24:42 +0530

    Added a function named addTask

--- src/App.js
+++ src/App.js
@@ -11,21 +11,45 @@ function App() {
 
 
 class Todo extends React.Component{
-  constructor() {
-  super();
+  constructor(props) {
+  super(props);
+    this.state={
+      tasklist:[],
+      text:"",
+      date:""
+    };
+    this.addTask = this.addTask.bind(this);
   }
+
+  addTask(){
+    var name = this.state.text
+    console.log(name)
+    if(name.length === 0){
+      alert("Task feild cannot be empty")
+    }
+    var date = this.state.date
+    console.log(date)
+    if(date.length === 0){
+      alert("Date feild canot be empty")
+    }
+    var dict={}
+    dict["name"]=name;
+    dict["date"]=date;
+    this.setState({
+      tasklist:[...this.state.tasklist,dict]
+    });
+  }
+  
   render(){
     return(
       <div>
         <h1>Todo List</h1>
-        <input type="text" placeholder="You task here"></input>&nbsp;
+        <input type="text" placeholder="Enter your task"></input>&nbsp;
         <input type="date"></input>&nbsp;
-        <button>Add task</button>
+        <button onClick={this.addTask}>Add task</button>
       </div>
     );
   }
 }
-
-
 
 export default App;
